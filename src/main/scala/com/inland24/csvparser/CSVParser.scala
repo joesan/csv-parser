1b0e52d3bb049ea424d9a0fea508b9bc4aac18f0
Jothi <jothi@Jothis-MacBook-Pro.local>
2017-04-26 22:33:15 +0200

    added csv parser fixes

--- src/main/scala/com/inland24/csvparser/CSVParser.scala
+++ src/main/scala/com/inland24/csvparser/CSVParser.scala
@@ -1,6 +1,5 @@
 package com.inland24.csvparser
 
-import com.opencsv.{ CSVReader => OpenCSVReader }
 import shapeless._
 
 import scala.io.Source
@@ -13,22 +12,24 @@ object CSVParser extends App {
 
   // This will be our interface between the Shapeless HList and our case class
   trait CSVRowParser[A] {
-    def parse[H <: HList](row: List[String])
-     (implicit gen: Generic.Aux[A, H], read: CSVRowReader[H]): Try[A] = {
-       // did you ever see it uglier than this?
-       // all I'm doing here is read the CSV row which is a List[String] using the implicitly
-       // provided Reader instance which in this case is the CSVRowReader[H] (actually an instance of it)
-       // and map that to my case class type supplied via the Generic interface from the Shapeless library
-       read from row map gen.from
+    def parse(row: List[String]): Try[A]
+  }
+
+  object CSVRowParser {
+    implicit def all[A, R <: HList](implicit gen: Generic.Aux[A, R],
+                                    read: CSVRowReader[R]): CSVRowParser[A] = new CSVRowParser[A] {
+      def parse(row: List[String]): Try[A] = {
+        read.from(row).map(gen.from)
+      }
     }
+    def apply[A](implicit ev: CSVRowParser[A]): CSVRowParser[A] = ev
   }
 
   // this is our case class that we will parse into
   case class User(id: Int, firstName: String, lastName: String)
 
-  def csvRowParser[A] = new CSVRowParser[A] {}
-
-  class CSVReader[A] {
+  class CSVReader[A: CSVRowParser] {
+    val defaultParserCfg = CSVParserConfig(Comma)
 
     def parse(path: String): ReaderWithFile[A] = ReaderWithFile[A](path)
 
@@ -36,41 +37,25 @@ object CSVParser extends App {
       implicit def parser2parsed[B](parser: ReaderWithFile[B]): Seq[B] = parser.using(defaultParserCfg)
     }
 
-    case class ReaderWithFile[B](path: String) {
-
-      // import the CSVRowReaders in scope
-      import CSVRowReader._
-
+    case class ReaderWithFile[B: CSVRowParser](path: String) {
       def using(cfg: CSVParserConfig): Seq[B] = {
         val lines = Source.fromFile(path).getLines()
-        while(lines.hasNext) {
+        while (lines.hasNext) {
           // we transform
           val splitted = lines.next.split(cfg.seperator.seperator).toList
-          val line = csvRowParser[B].parse(splitted)
+          val line = CSVRowParser[B].parse(splitted)
           println(line)
         }
-        println(cfg)   // line 2
+        println(cfg) // line 2
         null
       }
     }
   }
 
-  def apply[A] = new CSVReader[A]
+  def apply[A: CSVRowParser] = new CSVReader[A]
 
   val reader = apply[User]
 
-  val openCSVReader = new OpenCSVReader(new java.io.FileReader("/Users/jothi/Projects/Private/scala-projects/csv-parser/test.csv"))
-
-  import scala.collection.JavaConverters._
-  openCSVReader.readAll.asScala.foreach {
-    case elem => {
-      val user = csvRowParser[User].parse(elem.toList)
-    }
-  }
-
-  //val withDefaultCfg: Seq[User] = reader parse "/Users/jothi/Projects/Private/scala-projects/csv-parser/test.csv"
-
-  // but this works
-  val withCustomCfg = reader parse "/Users/jothi/Projects/Private/scala-projects/csv-parser/test.csv" using CSVParserConfig(Pipe)
-
+  val withDefaultCfg: Seq[User] = reader parse "/Users/jothi/Projects/Private/scala-projects/csv-parser/test.csv"
+  val withCustomConfig: Seq[User] = reader parse "/Users/jothi/Projects/Private/scala-projects/csv-parser/test.csv" using CSVParserConfig(Pipe)
 }
