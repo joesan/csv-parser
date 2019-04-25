e33c100f6a9bb8b8231a15457cdc4d7cb00f81a3
Joesan <codeintheopen@gmail.com>
2019-04-25 21:05:44 +0200

    removed unwanted files

--- src/test/scala/com/bigelectrons/csvparser/CSVParserTest.scala
+++ src/test/scala/com/bigelectrons/csvparser/CSVParserTest.scala
@@ -1,7 +1,7 @@
 package com.bigelectrons.csvparser
 
 import org.joda.time.DateTime
-import com.bigelectrons.csvparser.DummyParser.CSVRowParser._
+import com.bigelectrons.csvparser.CsvParser.CsvRowParser._
 import org.scalatest.FlatSpec
 
 
@@ -11,21 +11,9 @@ class CSVParserTest extends FlatSpec {
     val canonicalName = Some(classOf[MeterData].getCanonicalName)
     val meterCsvParserCfg = CSVParserConfig(withHeaders = true, caseClassCanonicalName = canonicalName, splitterFn = Some(meterDataSplitter))
     val meterCsv = "/Users/joesan/Projects/Private/scala-projects/csv-parser/src/test/resources/meter.csv"
-    //val meterDataSeq: Seq[MeterData] = meterDataReader parse meterCsv using meterCsvParserCfg
     val csvParser1 = CsvParser.apply[MeterData]
     val meterDataSeq: Seq[MeterData] = csvParser1.parse(meterCsv, meterCsvParserCfg)
     meterDataSeq foreach println
-
-
-
-    //val meterDataMapSeq: Seq[MeterDataAsMap] = meterDataMapReader parse "/Users/joesan/Projects/Private/scala-projects/csv-parser/src/test/resources/meter.csv" using (CSVParserConfig(withHeaders = true), fn = Some(meterDataSplitter))
-    //meterDataMapSeq foreach println
-
-    //val result = CsvParser1[User].parseCSVFile("/Users/joesan/Projects/Private/scala-projects/csv-parser/user.csv")
-    //result foreach println
-
-    //val userSeq: Seq[User] = userReader parse "/Users/joesan/Projects/Private/scala-projects/csv-parser/user.csv" using CSVParserConfig(withHeaders = true)
-    //userSeq foreach println
   }
 
   // this is our case class that we will parse into
@@ -36,20 +24,11 @@ class CSVParserTest extends FlatSpec {
   case class MeterData(meterId: String, dateTime: DateTime, meterReadings: Seq[Double])
   case class MeterDataAsMap(meterId: String, dateTime: DateTime, meterReadings: Map[String, Double])
 
-  // TODO: We need this header to be resolved right here... otherwise it seems not to work! This is a dummy header just for testing!
-  implicit val headers: Seq[String] = Seq("a", "b", "c", "d")
-
-  val meterDataReader = DummyParser.apply[MeterData]
-  val meterDataMapReader = DummyParser.apply[MeterDataAsMap]
-  val userReader = DummyParser.apply[User]
-
   // Custom logic to split MeterData
   def meterDataSplitter(s: Seq[String]): Seq[String] = {
     Seq(s.head, s(1), s.drop(2).mkString(Comma.separator))
   }
-
-
-
+  
 //val withCustomConfig: Seq[Address] = reader parse "/Users/jothi/Projects/Private/scala-projects/csv-parser/address.csv" using CSVParserConfig(Pipe)
 //withCustomConfig foreach println
 /*
